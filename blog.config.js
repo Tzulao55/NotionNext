c7f8c5af2a9688584c0fddeaffa8ca9bf0ec0d98
Lyn <rosediving@gmail.com>
2025-12-02 21:48:14 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -65,7 +65,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我是一个程序员, Hi，我是一个打工人,Hi，我是一个干饭人,欢迎来到我的博客🎉',
+    '读万卷书行万里路, 愿你在英国学有所成, 享受探索世界的乐趣, 期待你的精彩分享',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
